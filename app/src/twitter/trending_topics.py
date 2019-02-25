1e06aa9774af45ca8a1945303196afa77fbdeae1
Daniel Milenkovic <flipbug360@gmail.com>
2019-02-25 13:23:09 +0100

    Push current wip

--- app/src/twitter/trending_topics.py
+++ app/src/twitter/trending_topics.py
@@ -1,4 +1,14 @@
 from api import api
 
-trendingTopics = api.GetTrendsCurrent()
-print(trendingTopics)
+chWoeid = 23424957
+trendingTopics = api.GetTrendsWoeid(chWoeid)
+
+trendingTopicsWithTweets = {}
+for topic in trendingTopics:
+    tweets = api.GetSearch(term=topic.name)
+    trendingTopicsWithTweets[topic.name] = [t.AsDict() for t in tweets]
+
+    # break for testing reasons    
+    break
+
+print(trendingTopicsWithTweets)
