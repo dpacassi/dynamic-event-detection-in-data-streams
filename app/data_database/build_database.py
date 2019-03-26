77d11751d20b25629e894fcb3985856c48ce6b88
David Pacassi Torrico <dpacassi@gmail.com>
2019-03-26 09:10:09 +0100

    Add a script to build the news database

---
+++ app/data_database/build_database.py
@@ -0,0 +1,46 @@
+'''
+from newspaper import Article
+
+url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+article = Article(url)
+
+article.download()
+article.parse()
+article.nlp()
+
+print(article.meta_lang)
+'''
+
+import os
+import csv
+import pymysql
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+connection = pymysql.connect(
+  host=os.environ['MYSQL_HOSTNAME'],
+  port=int(os.environ['MYSQL_PORT']),
+  user=os.environ['MYSQL_USER'],
+  passwd=os.environ['MYSQL_PASSWORD'],
+  database=os.environ['MYSQL_DATABASE'],
+  charset='utf8mb4',
+  cursorclass=pymysql.cursors.DictCursor
+)
+sql = "INSERT INTO news_article (id, title, url, publisher, category, story, hostname, date, newspaper_processed, newspaper_meta_language, newspaper_keywords, newspaper_text) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+data = []
+
+with open('data/uci-news-aggregator.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
+
+    # Skip the header row.
+    next(csv_reader, None)
+
+    for row in csv_reader:
+        data.append((row[0], row[1], row[2], row[3], row[4], row[5], row[6], datetime.fromtimestamp(int(row[7])/1000), 1, None, None, None))
+
+with connection.cursor() as cursor:
+    cursor.executemany(sql, data)
+
+connection.commit()
---
+++ app/data_database/create_database.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE `data_database` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE `data_database`.`news_article` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(1023) NULL,
+  `url` VARCHAR(1023) NULL,
+  `publisher` VARCHAR(255) NULL,
+  `category` VARCHAR(255) NULL,
+  `story` VARCHAR(255) NULL,
+  `hostname` VARCHAR(255) NULL,
+  `date` DATETIME NULL,
+  `newspaper_processed` TINYINT UNSIGNED NOT NULL,
+  `newspaper_meta_language` VARCHAR(16) NULL,
+  `newspaper_keywords` LONGTEXT NULL,
+  `newspaper_text` LONGTEXT NULL,
+  PRIMARY KEY (`id`)
+);
