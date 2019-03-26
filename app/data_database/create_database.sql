16f6205f26e5e30715ef2186094895dd40afd265
David Pacassi Torrico <dpacassi@gmail.com>
2019-03-26 09:40:00 +0100

    Update create statement

--- app/data_database/create_database.sql
+++ app/data_database/create_database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE `data_database` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 
 CREATE TABLE `data_database`.`news_article` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(1023) NULL,
+  `title` VARCHAR(255) NULL,
   `url` VARCHAR(1023) NULL,
   `publisher` VARCHAR(255) NULL,
   `category` VARCHAR(255) NULL,
