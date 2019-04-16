807b8e21288cb1e0b3ce8225c66e3526dccf8259
David Pacassi Torrico <dpacassi@gmail.com>
2019-04-16 19:26:41 +0200

    Fix DB table creation query

--- app/data_processing/define_cluster_evaluations.py
+++ app/data_processing/define_cluster_evaluations.py
@@ -21,9 +21,16 @@ connection = pymysql.connect(
 )
 
 truncate_sql = "TRUNCATE TABLE cron_evaluation"
-
+insert_sql = (
+    "INSERT INTO cron_evaluation"
+    " (method, rows, skip_rows, vectorizer, tokenizer, parameters, normalized_mutual_info_score, adjusted_mutual_info_score, completeness_score, estimated_clusters, real_clusters, n_noise, time_clustering, time_preprocessing, time_total, processed, failed)"
+    " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+)
 
 with connection.cursor() as cursor:
     # Truncate our table.
     cursor.execute(truncate_sql)
     connection.close()
+
+    # Insert hdbscan calls.
+
