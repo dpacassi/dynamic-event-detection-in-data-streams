8ed55af389dd04d45f0f49a85d62873e19c1fe34
David Pacassi Torrico <dpacassi@gmail.com>
2019-06-04 15:23:16 +0200

    Fix cropped images

--- app/data_processing/plots_summary_resize.py
+++ app/data_processing/plots_summary_resize.py
@@ -3,7 +3,7 @@ from PIL import Image
 new_width = 625
 new_height = 448
 
-def resize_image(image_file, new_image_file):
+def resize_image(image_file, new_image_file, fix=False):
     im = Image.open(image_file)
     width, height = im.size  # Get dimensions
 
@@ -12,10 +12,14 @@ def resize_image(image_file, new_image_file):
     right = (width + new_width) / 2
     bottom = (height + new_height) / 2
 
+    if fix:
+        top += 12
+        bottom += 12
+
     im = im.crop((left, top, right, bottom))
     im.save(new_image_file)
 
 resize_image("../../doc/images/summary/event_detection_differences_de.png", "../../doc/images/summary/cropped_event_detection_differences_de.png")
 resize_image("../../doc/images/summary/event_detection_differences_en.png", "../../doc/images/summary/cropped_event_detection_differences_en.png")
-resize_image("../../doc/images/summary/hdbscan_kmeans_de.png", "../../doc/images/summary/cropped_hdbscan_kmeans_de.png")
-resize_image("../../doc/images/summary/hdbscan_kmeans_en.png", "../../doc/images/summary/cropped_hdbscan_kmeans_en.png")
+resize_image("../../doc/images/summary/hdbscan_kmeans_de.png", "../../doc/images/summary/cropped_hdbscan_kmeans_de.png", True)
+resize_image("../../doc/images/summary/hdbscan_kmeans_en.png", "../../doc/images/summary/cropped_hdbscan_kmeans_en.png", True)
